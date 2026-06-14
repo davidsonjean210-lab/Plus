1f302b77457eb2e291ff7aed6b54e2602f5144ba
davidsonjean210-lab <davidsonjean210@gmail.com>
2026-06-14 08:44:00 -0400

    Actualizar script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getFirestore(app);t
 const auth = getAuth(app);
 const storage = getStorage(app);
 
